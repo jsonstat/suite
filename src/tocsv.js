9db228d8cc0c789c410399b47a0e741ef68770dd
Xavier Badosa <xbadosa@gmail.com>
2021-06-22 14:19:26 +0200

    3.2.1
    Experimental array option.

--- src/tocsv.js
+++ src/tocsv.js
@@ -6,136 +6,148 @@ import dcomma from "./dcomma.js";
 //jsonstat, {rich, dsid, delimiter, decimal, na, [ignored if rich: vlabel, slabel, status], [ignored if not rich: separator]}
 //Returns text (CSV or JSV[JSON-stat Comma Separed values or "CSV-stat" -Rich CSV-])
 export default function toCSV(jsonstat, options){
-		if(typeof jsonstat==="undefined"){
-			return null;
-		}
+	if(typeof jsonstat==="undefined"){
+		return null;
+	}
 
-		if(typeof options==="undefined"){
-			options={};
-		}
+	if(typeof options==="undefined"){
+		options={};
+	}
 
-		var
-			csv="",
-			header="jsonstat",
-			rich=(options.rich===true), //2.3.0 Default: false (backward compat)
+	var
+		header="jsonstat",
+		rich=(options.rich===true), //2.3.0 Default: false (backward compat)
 
-			//The following options are ignored when rich
-			//When rich, toTable uses field=id and vlabel/slabel are ignored
-			vlabel=rich ? "value" : (options.vlabel || "Value"), //Same default as .toTable()
-			slabel=rich ? "status" : (options.slabel || "Status"), //Same default as .toTable()
-			status=(options.status===true), //Same default as .toTable(). If rich, it will be rewritten and set according to ds content
+		//The following options are ignored when rich
+		//When rich, toTable uses field=id and vlabel/slabel are ignored
+		vlabel=rich ? "value" : (options.vlabel || "Value"), //Same default as .toTable()
+		slabel=rich ? "status" : (options.slabel || "Status"), //Same default as .toTable()
+		status=(options.status===true), //Same default as .toTable(). If rich, it will be rewritten and set according to ds content
 
-			na=options.na || "n/a",
+		na=options.na || "n/a",
 
-			delimiter=options.delimiter || ",",
-			separator=options.separator || "|", //2.3.0 only if rich
-			decimal=(delimiter===";") ?
-				(options.decimal || ",")
-				:
-				(options.decimal || "."),
-			dsid=options.dsid || 0,
-			ds=dataset(jsonstat, dsid)
-		;
+		delimiter=options.delimiter || ",",
+		separator=options.separator || "|", //2.3.0 only if rich
+		decimal=(delimiter===";") ?
+			(options.decimal || ",")
+			:
+			(options.decimal || "."),
 
-		if(!checkds(ds)){
-			return null;
-		}
+		array=(options.array===true), //experimental
 
-		//If rich, include status if available
-		if(rich){
-			status=(ds.status!==null);
-		}
+		dsid=options.dsid || 0,
+		ds=dataset(jsonstat, dsid),
 
-		var
-			table=ds.toTable({
-				vlabel: vlabel,
-				slabel: slabel,
-				status: status,
-				field: rich ? "id" : "label",
-				content: rich ? "id" : "label",
-				type: "array"
-			}),
-			vcol=table[0].indexOf(vlabel),
-			scol=status ? table[0].indexOf(slabel) : -1
-		;
+		csv=array ? [] : "" //experimental
+	;
 
-		table.forEach(function(r, j){
-			r.forEach(function(c, i){
-				if(j && i===vcol){
-					if( c===null ){
-						r[i]=dcomma(na,delimiter);
-					}else{
-						if(decimal!=="."){
-							r[i]=String(r[i]).replace(".", decimal);
-						}
-					}
+	if(!checkds(ds)){
+		return null;
+	}
+
+	//If rich, include status if available
+	if(rich){
+		status=(ds.status!==null);
+	}
+
+	var
+		table=ds.toTable({
+			vlabel: vlabel,
+			slabel: slabel,
+			status: status,
+			field: rich ? "id" : "label",
+			content: rich ? "id" : "label",
+			type: "array"
+		}),
+		vcol=table[0].indexOf(vlabel),
+		scol=status ? table[0].indexOf(slabel) : -1
+	;
+
+	table.forEach(function(r, j){
+		r.forEach(function(c, i){
+			if(j && i===vcol){
+				if( c===null ){
+					r[i]=dcomma(na,delimiter);
 				}else{
-					if(j && i===scol && c===null){
-						r[i]=""; //Status does not use n/a because usually lacking of status means "normal".
-					}else{
-						r[i]=dcomma(r[i],delimiter);
+					if(decimal!=="."){
+						r[i]=String(r[i]).replace(".", decimal);
 					}
 				}
-			});
-
-			csv+=r.join(delimiter)+"\n";
+			}else{
+				if(j && i===scol && c===null){
+					r[i]=""; //Status does not use n/a because usually lacking of status means "normal".
+				}else{
+					r[i]=dcomma(r[i],delimiter);
+				}
+			}
 		});
 
-		if(rich){
-			header+=delimiter+decimal+delimiter+separator+"\n";
-			["label", "source", "updated", "href"].forEach(function(s){
-				if(ds[s]){
-					header+=s+delimiter+dcomma(ds[s],delimiter)+"\n";
-				}
-			});
+		if(array){
+			csv.push(r.join(delimiter)); //experimental
+		}else{
+			csv+=r.join(delimiter)+"\n";
+		}
+	});
 
-			//dimensions
-			ds.id.forEach(function(e,i){
+	if(rich){
+		header+=delimiter+decimal+delimiter+separator+"\n";
+		["label", "source", "updated", "href"].forEach(function(s){
+			if(ds[s]){
+				header+=s+delimiter+dcomma(ds[s],delimiter)+"\n";
+			}
+		});
+
+		//dimensions
+		ds.id.forEach(function(e,i){
+			var
+				unit=[],
+				dim=ds.Dimension(i),
+				role=dim.role,
+				hasUnit=false
+			;
+
+			header+="dimension"+delimiter+dcomma(e,delimiter)+delimiter+dcomma(dim.label,delimiter)+delimiter+dim.length;
+
+			if(role==="metric" && dim.__tree__.category.unit){
+				hasUnit=true;
+			}
+
+			//categories
+			dim.id.forEach(function(e,i){
 				var
-					unit=[],
-					dim=ds.Dimension(i),
-					role=dim.role,
-					hasUnit=false
+					u=[],
+					cat=dim.Category(i)
 				;
-
-				header+="dimension"+delimiter+dcomma(e,delimiter)+delimiter+dcomma(dim.label,delimiter)+delimiter+dim.length;
-
-				if(role==="metric" && dim.__tree__.category.unit){
-					hasUnit=true;
-				}
-
-				//categories
-				dim.id.forEach(function(e,i){
-					var
-						u=[],
-						cat=dim.Category(i)
-					;
-					header+=delimiter+dcomma(e,delimiter)+delimiter+dcomma(cat.label,delimiter);
-					if(hasUnit){
-						u.push(
-							cat.unit.hasOwnProperty("decimals") ? cat.unit.decimals : ""
-						);
-						u.push(cat.unit.label||"");
-						if(cat.unit.symbol){
-							u.push(cat.unit.symbol);
-							u.push(cat.unit.position);
-						}
-						unit.push(dcomma( u.join(separator), delimiter));
+				header+=delimiter+dcomma(e,delimiter)+delimiter+dcomma(cat.label,delimiter);
+				if(hasUnit){
+					u.push(
+						cat.unit.hasOwnProperty("decimals") ? cat.unit.decimals : ""
+					);
+					u.push(cat.unit.label||"");
+					if(cat.unit.symbol){
+						u.push(cat.unit.symbol);
+						u.push(cat.unit.position);
 					}
-				});
-
-				if(role!==null && role!=="classification"){
-					header+=delimiter+dim.role;
-					if(hasUnit){
-						header+=delimiter+unit.join(delimiter);
-					}
+					unit.push(dcomma( u.join(separator), delimiter));
 				}
-
-				header+="\n";
 			});
 
+			if(role!==null && role!=="classification"){
+				header+=delimiter+dim.role;
+				if(hasUnit){
+					header+=delimiter+unit.join(delimiter);
+				}
+			}
+
+			header+="\n";
+		});
+
+		if(array){
+			csv=header.concat(["data"], csv); //experimental
+		}else{
 			csv=header+"data\n"+csv;
 		}
-
-		return csv;
 	}
+
+	return csv;
+}
